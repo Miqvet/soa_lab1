21cfcbfcbf083aa2ada480bba6f7af688b67dea7
foliageh <mygazztop1@gmail.com>
2025-10-11 02:13:54 +0300

    swagger, fix flats api url

--- flats-app/src/api/flats.js
+++ flats-app/src/api/flats.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
+const API_BASE_URL = 'http://localhost:8080' // Замените на ваш URL
+
 const api = axios.create({
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
